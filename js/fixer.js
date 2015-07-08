8b64c609cc253f58ea8840f673b64b03f36c4650
Alex <alex@MacMini1.local>
2015-07-08 11:33:07 -0400

    when an item has been selected, it's color is changed to yellow

--- js/fixer.js
+++ js/fixer.js
@@ -7,18 +7,27 @@
 		remove the active class from the child list item
 */
 $('.optionList').on('click', function(){
+
 	var $optionListId = $(this).attr('id'); //gets this optionlist's id
 	console.log($optionListId + ' optionList has been accessed');
 
+
 	$('.optionList').removeClass('active');
 	console.log($optionListId + '\'s active class removed');
-
 	$(this).addClass('active');
-	console.log($optionListId + ' now has class active');
+	console.log($optionListId + ' now has an li with class active');
 
-	if( $('.active', this).length() > 0 ){
+
+	$('ul ul li').on('click', function(){
+		$('li').removeClass('activated');
+		$(this).addClass('activated');
+	});
+
+	//suppose to remove all li's from the optionlists
+	if( $('.active', this).length > 0 ){
 		$('li', this).removeClass('active');
-	}
+	} 
+
 });
 
 
@@ -47,12 +56,41 @@ if click arrow and not first 2 then do next,prev slector and toggle class
 /********************
 My Take For Arrows
 /********************/
+/*
 
 when .rightSelect is clicked
-	remove active class of prev 'active'
-	traverse to sibling or next li
-	.addclass 'active' to that slibling or next li
-	assign href of current active li to boxwrap img src attr
+	
+	if the end of the ul has been reached
+		remove 'active' class from current li
+		traverse the to first child in the ul
+		addclass 'active' to first child in ul
+		assign href of current active li to boxwrap
+*/
+
+	$('.rightSelect').on('click', function(){
+		var activeUL = $('li .active').attr('class'); //gets the id of the ul with active array
+		console.log(activeUL);
+		var sTLength = $('#mockup li').length; //holds max number of li
+
+
+	});
+
+/*
+	if the previous adjacent sibling has class active
+		remove active class of prev li
+		traverse to sibling or next li
+		.addclass 'active' to that slibling or next li
+		assign href of current active li to boxwrap img src attr
+	otherwise (else) if the next adjacent sibling has class active
+		remove active class from next li
+		traverse to sibling or prev li
+		.addclass 'active' to that sibling or prev li
+		assign href of current active li to boxwrap img src attr
+*/
+
+
+
+
 
 
 
